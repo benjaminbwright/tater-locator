4aa1b175d004b1890202432d70524217bc6006b4
Benjamin Wright <benjamin@famousstick.com>
2019-07-07 21:07:09 -0400

    Added the refresh icon to the footer UI.

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Header } from '../Header/Header';
 import TotMap from '../TotMap/TotMap';
+import { FooterUI } from '../FooterUI/FooterUI';
 
 class App extends Component {
 
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App page bg-white">
         <Header />
         <TotMap id="TotMap" />
+        <FooterUI />
       </div>
     );
   }
